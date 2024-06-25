79620b7e19e817fc49cf214dd0fd5a8e2aee975e
pavan <balubadi.pavankumar@gmail.com>
2024-06-25 22:02:30 +0530

    Jenkinsfile -DSL

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,24 +1,26 @@
-pipeline {
-    agent any
-    stages {
-        stage('Stage 1') {
-            steps {
-                echo 'Hello world 12!'
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage('Stage 1') {
+                steps {
+                    echo 'Hello world 12!'
+                }
             }
-        }
-        stage('Stage 2') {
-            steps {
-                echo 'Hello world 23!'
+            stage('Stage 2') {
+                steps {
+                    echo 'Hello world 23!'
+                }
             }
-        }
-        stage('Stage 3') {
-            steps {
-                echo 'Hello world 34!'
+            stage('Stage 3') {
+                steps {
+                    echo 'Hello world 34!'
+                }
             }
-        }
-        stage('Stage 4') {
-            steps {
-                echo 'Hello world 4!'
+            stage('Stage 4') {
+                steps {
+                    echo 'Hello world 4!'
+                }
             }
         }
     }
